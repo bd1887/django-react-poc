66b8bc4a6579de46e8001052c9e18c3be7b1769e
bd1887 <bd1887@gmail.com>
2019-03-19 17:36:50 +0000

    removed pyodbc

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from app import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("page-one/", views.index, name="index"),
-    path("page-two/", views.index, name="index"),
+    path("students/", views.index, name="index"),
+    path("exams/", views.index, name="index"),
+    path("chart/", views.index, name="index"),
     
 ]

cf4a1556131b3cca27fbd82ab48a9544689fb1cf
bd1887 <bd1887@gmail.com>
2019-03-21 19:02:24 +0000

    pypyodbc test

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view
 from api.azure_ml import azure_test
-import pyodbc
+import pypyodbc
 # Create your views here.
 
 @api_view(['GET'])
@@ -19,7 +19,7 @@ def azure_test(response):
 
 @api_view(['GET'])
 def db_test(response):
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=natheandemos.database.windows.net;DATABASE=RianProjectDB1;UID=DIT265A_USR;PWD=479M1LZ5Uk')
+    cnxn = pypyodbc.connect('DRIVER={SQL Server};SERVER=natheandemos.database.windows.net;DATABASE=RianProjectDB1;UID=DIT265A_USR;PWD=479M1LZ5Uk')
     cursor = cnxn.cursor()
     # get one record from the database and print it 
     cursor.execute("select id, name from tbl_D17125388")
